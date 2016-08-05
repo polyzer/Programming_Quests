12cf04c67b7c57286490b8a092e69fda80eeb5ca
polyzer <realpolyzer@yandex.ru>
2016-08-05 15:45:32 +0500

    Update quest_3_BigNums.c

--- quest_3_BigNums.c
+++ quest_3_BigNums.c
@@ -12,31 +12,44 @@ typedef struct _BigNumber{
 BigNumber *createBigNum(const char *Str);
 BigNumber *addBigNums(const BigNumber *BN1, const BigNumber *BN2);
 void printBigNum(BigNumber *BN);
+void freeBN(BigNumber **);
 
 int main (int argc, char **argv)
 {
 	BigNumber *BN1;
 	BigNumber *BN2;
 	BigNumber *BN3;
+	int i;
 
 	setlocale(LC_ALL, "Russian");
 	
-	BN1 = createBigNum("0xFFFFFFFF");
+	i = 0;
+	BN1 = createBigNum("0x3333333FDA751531247652394782398746BCB23765437825423478523485234");
 	printBigNum(BN1);
-	BN2 = createBigNum("1");
+	printf("\n");
+	BN2 = createBigNum("0x4242AAAAAAAA48762348765FACBDDDA4325AA");
 	printBigNum(BN2);
+	printf("\n");
 	BN3 = addBigNums(BN2, BN1);
+	printBigNum(BN3);	
+	for(i = 0; i < 100; i++)
+	{
+		BN3 = addBigNums(BN3, BN1);
+	}
+
 	printBigNum(BN3);
-
-	free(BN1);
-	BN1 = NULL;
-	free(BN2);
-	BN2 = NULL;
-	free(BN3);
-	BN3 = NULL;
-
+	freeBN(&BN1);
+	freeBN(&BN2);
+	freeBN(&BN3);
+	
 	system("pause");
 	return 0;
+}
+
+void freeBN(BigNumber** BigNum)
+{
+	free((*BigNum)->number);
+	free(*BigNum);
 }
 
 BigNumber *createBigNum(const char *cStr)
@@ -48,8 +61,8 @@ BigNumber *createBigNum(const char *cStr)
 	BigNumber * NewNum;
 	unsigned long long i,k, rest;
 	
-	Str = (char *) malloc(strlen(cStr) + 1);
-	strcpy(Str, cStr);
+	Str = (char *) malloc(strlen(cStr) - 1);
+	strcpy(Str, cStr + 2);
 	Str[strlen(cStr)] = 0;
 	
 	StrLen = strlen(Str);
@@ -57,68 +70,36 @@ BigNumber *createBigNum(const char *cStr)
 	NewNum = (BigNumber *) malloc(sizeof(BigNumber));
 	i = 0;
 
-	// прворяем - 16 ричное ли число!
-	if ((Str[1] == 'x' || Str[1] == 'X') && (Str[0] == '0') || isalpha(Str[0]))
-	{
-		if((Str[1] == 'x' || Str[1] == 'X') && (Str[0] == '0'))
-		{
-			Str[1] = '0';
-			Str[0] = '0';
-		}
 		// выделяем память
 		NewNum->number = (unsigned int *) malloc(sizeof(unsigned int) * (StrLen/8 + 1));
 		// сколько букв не влезло в целое слово
 		rest = StrLen % 8;
+		if(rest == 0)
+			NewNum->length = (StrLen/8);
+		else
+			NewNum->length = (StrLen/8)+1;
+		// заполняем 0 все значения.
+		for(i = 0; i < NewNum->length; i++)
+			NewNum->number[i] = 0;
+		i = 0;
 		// заполняем слова!
 		while(StrLen > 7)
 		{
 			for(k = 0; k < 8; k++)
 			{
-				tstr[7-k] = Str[StrLen - k];
+				tstr[7-k] = Str[StrLen - k - 1];
 			}
 			StrLen -= 8;
 			tstr[k] = 0;
 			NewNum->number[i] = strtoul(tstr, NULL, 16);
 			i++;
 		}
-		for(k = 0; k < StrLen; k++)
-		{
-			tstr[k] = Str[k];
-		}
-		tstr[k] = 0;
-		NewNum->number[i] = strtoul(tstr, NULL, 16);
-
-	}else
-	{
-		// выделяем память
-		NewNum->number = (unsigned int *) malloc(sizeof(unsigned int) * (StrLen/10 + 1));
-		// сколько букв не влезло в целое слово
-		rest = StrLen % 8;
-		// заполняем слова!
-		while(StrLen > 10)
-		{
-			for(k = 0; k < 10; k++)
+			for(k = 0; k < rest; k++)
 			{
-				tstr[9-k] = Str[StrLen - k];
+				tstr[k] = Str[k];
 			}
-			StrLen -= 10;
 			tstr[k] = 0;
-			// переводим в число и проверяем на прееполнение
-			if((NewNum->number[i] = strtoul(tstr, NULL, 10)) && NewNum->number[i] == UINT_MAX)
-			{
-				tstr[0] = '0';
-				NewNum->number[i] = strtoul(tstr, NULL, 10);
-			}
-			i++;
-		}
-		for(k = 0; k < StrLen; k++)
-		{
-			tstr[k] = Str[k];
-		}
-		tstr[k] = 0;
-		NewNum->number[i] = strtoul(tstr, NULL, 10);
-
-	}
+			NewNum->number[i] = strtoul(tstr, NULL, 16);
 
 	return NewNum;
 }
@@ -129,6 +110,7 @@ BigNumber *addBigNums(const BigNumber *BN1, const BigNumber *BN2)
 	unsigned long long i, carry, rest;
 	BigNumber *Res;
 	i = 0;
+	carry = 0;
 	// то, с чем будем сравнивать
 	Res = (BigNumber *) malloc(sizeof(BigNumber));
 	if(BN1->length >= BN2->length){
@@ -139,50 +121,56 @@ BigNumber *addBigNums(const BigNumber *BN1, const BigNumber *BN2)
 		Bigger = BN2;
 		Smaller = BN1;
 	}
+	Res->number = (unsigned int *) malloc(sizeof(unsigned int) * (Bigger->length + 1));
 	// складываем сначала оба числа
 		for(i=0; i < Smaller->length; i++)
 		{
 			if(BN1->number[i] > INT_MAX && BN2->number[i] > INT_MAX)
 			{
+				Res->number[i] = (BN1->number[i] - INT_MAX) + (BN2->number[i] - INT_MAX)  + carry - 2;
 				carry = 1;
-				// ТУТ МОЖЕТ БЫТЬ КОСЯК!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				Res->number[i] = (BN1->number[i] - INT_MAX) + (BN2->number[i] - INT_MAX)  + carry + 1;
-			} else if(BN1->number[i] > INT_MAX && BN2->number[i] < INT_MAX)
+				// если у нас первое число меньше второго
+			} else if(BN1->number[i] > INT_MAX && BN2->number[i] <= INT_MAX)
 			{
-				// 
-				if((BN1->number[i] - INT_MAX) + BN2->number[i] + carry > INT_MAX)
+				if((BN1->number[i] - INT_MAX) + BN2->number[i] + carry >= INT_MAX)
 				{
+					Res->number[i] = (BN1->number[i] - INT_MAX) + BN2->number[i] + carry - INT_MAX - 2;
 					carry = 1;
-					Res->number[i] = (BN1->number[i] - INT_MAX) + BN2->number[i] + carry;
 				}else
 				{
+					Res->number[i] = BN1->number[i] + BN2->number[i] + carry;
 					carry = 0;
-					Res->number[i] =  (BN1->number[i] - INT_MAX) + BN2->number[i] + carry;
 				}
-			} else if(BN1->number[i] < INT_MAX && BN2->number[i] > INT_MAX)
+			} else if(BN1->number[i] <= INT_MAX && BN2->number[i] > INT_MAX)
 			{
 				// если у нас второе число меньше половины
-				if(BN1->number[i] + (BN2->number[i] - INT_MAX) + carry > INT_MAX)
+				if(BN1->number[i] + (BN2->number[i] - INT_MAX) + carry >= INT_MAX)
 				{
+					Res->number[i] = BN1->number[i] + (BN2->number[i] - INT_MAX) + carry - INT_MAX - 2;
 					carry = 1;
-					Res->number[i] = BN1->number[i] + (BN2->number[i] - INT_MAX) + carry;
 				}else
 				{
+					Res->number[i] = BN1->number[i] + BN2->number[i] + carry;
 					carry = 0;
-					Res->number[i] = BN1->number[i] + (BN2->number[i] - INT_MAX) + carry;
 				}
-				// если у нас оба числа меньше половины
+				// если у нас оба числа меньше половины - то не будет переполнения
 			} else
 			{
+				Res->number[i] = BN1->number[i] + BN2->number[i] + carry;
 				carry = 0;
-				Res->number[i] = BN1->number[i] + (BN2->number[i] - INT_MAX) + carry;
 			}
 		}
 		// теперь копируем одно число + перенос, если есть
 		for(; i < Bigger->length; i++)
 		{
-			Res->number[i] += Res->number[i] + carry;
-			carry = 0;
+			if(Bigger->number[i] == 0xFFFFFFFF && carry == 1)
+			{
+				carry = 1;
+			} else
+			{
+				Res->number[i] = Bigger->number[i] + carry;
+				carry = 0;
+			}
 		}
 		// i++ уже увеличено, и если есть перенос, 
 		// при том, что мы уже все числа скопировали, то тогда создаем новый разряд
@@ -190,9 +178,9 @@ BigNumber *addBigNums(const BigNumber *BN1, const BigNumber *BN2)
 		if(carry == 1)
 		{
 			Res->number[i] = 1;
-			Res->length = i;
+			Res->length = i+1;
 		}else
-			Res->length = i-1;
+			Res->length = i;
 
 	return Res;
 }
@@ -200,7 +188,18 @@ BigNumber *addBigNums(const BigNumber *BN1, const BigNumber *BN2)
 void printBigNum(BigNumber *BN)
 {
 	unsigned long long i;
-	for(i = BN->length-1; i >= 0 ; i--)
-		printf("%u", BN->number[i]);
+	i = BN->length-1;
+	if(i == 0)
+	{
+		printf("%08X \n", BN->number[i]);
+		return;
+	}
+	while(1){
+		printf("%08X ", BN->number[i]);
+		if(i == 0)
+			break;
+		i--;
+	}
+	printf("\n");
 }
 
